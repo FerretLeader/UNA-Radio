fb3e354e2c358d18f885acc617f4ad6cc992ecaa
LeeBrummitt <42744568+LeeBrummitt@users.noreply.github.com>
2019-01-25 17:01:29 -0600

    added mongo node stuff

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,10 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "";
 const express = require('express');
+var mongojs = require('mongojs');
 const app = express();
 
 app.get('/', function(request, response) {  response.sendfile(__dirname + "/index.html");});  //index.html is a seperate file
 
 app.listen(8080);
+
